d7006a97711315fda4a8b3ce2c7b10d3af4afefe
Luispaim <40635713+Luispaim@users.noreply.github.com>
2021-02-03 22:04:01 -0300

    initial

---
+++ index.js
@@ -0,0 +1,81 @@
+const porta = 3003
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// validação
+const validateQuery = (req, res, next) => {
+    if (req.query.nome) {
+        next()
+        return
+    }
+    res.status(400).send("Parâmetro errado")
+}
+
+// Middleware
+app.use((req, res, next) => {
+    const produtos = bancoDeDados.getProdutos()
+    console.log(produtos)
+    if (produtos.find((p) => p.nome == req.body.nome)) {
+        res.status(400).send("Nome já cadastrado")
+        return
+    }
+    next()
+})
+
+// Endpoint Bem Vindo!!
+app.get('/', validateQuery, (req, res, next) => {
+  res.status(200).send(`Bem vindo, ${req.query.nome} !!`)
+})
+
+// Endpoint Listar Produtos
+app.get('/produtos', (req, res, next) => {
+    res.status(200).send(bancoDeDados.getProdutos())
+})
+
+// Endpoint Filtrar Produtos
+app.get('/produtos', validateQuery, (req, res, next) => {
+    res.status(200).send(bancoDeDados.getProdutosFilter(req.query.preco))
+})
+
+// Endpoint Listar Um Produto
+app.get('/produtos/:id', (req, res, next) => {
+    res.status(200).send(bancoDeDados.getProduto(req.params.id))
+})
+
+// Endpoint Criar Produto
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.status(200).send(produto) // JSON
+})
+
+// Endpoint Editar Produto
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.status(200).send(produto) // JSON
+})
+
+// Endpoint Deletar Produto
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.status(200).send(produto) // JSON
+})
+
+// Tratamento 404 Not Found
+app.use((req, res, next) => {
+  res.status(404).send("Página não encontrada")
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
